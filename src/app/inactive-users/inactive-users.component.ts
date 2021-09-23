ade67f9dd5a264c2464cded1b11e7accc62c2e97
Mateusz <wmateusz993@gmail.com>
2021-09-23 18:44:11 +0200

    add user service

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {UserService} from '../user.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+export class InactiveUsersComponent implements OnInit {
+  users: string[];
+
+  constructor(private servUse: UserService) {
+  }
+ngOnInit() {
+    this.users = this.servUse.inactiveUsers;
+}
+
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.servUse.onSetToActive(id);
   }
 }
